7fcfa5f4d95ff67c684dcaaf08f03b86ba93d1c3
Heiko <heiko.pruessing@gmx.de>
2022-11-27 20:45:54 +0100

    [CHANGE] reformat script

--- AbootConverter.py
+++ AbootConverter.py
@@ -3,16 +3,21 @@
 
 """Amiga Bridegboard autoboot harddisk file binary image converter.
    Converts a raw HDD image (e.g. from "DOSBox" or "bochs" DOS emulator
-   to a Amiga virtial PC disk HDD image usable from a Commodore Bridgeboard and
+   to a Amiga virtual PC disk HDD image usable from a Commodore Bridgeboard and
    vice versa)
 
    Written by Heiko Prüssing in 2021,2022
 """
 
-import sys, struct, fileinput, os, math
+import sys
+import struct
+import fileinput
+import os
+import math
 
 sectorSize = 512
 abootHeaderSize = 512
+
 
 def inputNumber(str, min, max, default) -> int:
     while True:
@@ -22,24 +27,27 @@ def inputNumber(str, min, max, default) -> int:
                 valstr = ("%d" % default)
             val = int(valstr)
             if val < min or val > max:
-                print("Error, the number must be in range [%d..%d]" % (min,max) )
+                print(
+                    "Error, the number must be in range [%d..%d]" % (min, max))
             else:
                 return val
         except ValueError:
             print("Please enter a valid integer!")
 
-def detectFlatHardDiskGeometry(filename) -> (int, int, int):
+
+def detectFlatHardDiskGeometry(filename): # -> (int, int, int):
     # Detect geometry of a flat harddisk file.
     # Take the file size and create a proper CHS for that.
     realFileSize = os.path.getsize(filename)
-    #print("realsize size = %d" % realFileSize)
-    cylinders = math.ceil( realFileSize / 16.0 / 63.0 / 512.0 )
-    #print("recalculated size = %d" % (cylinders * 16 * 63 * 512) )
+    # print("realsize size = %d" % realFileSize)
+    cylinders = math.ceil(realFileSize / 16.0 / 63.0 / 512.0)
+    # print("recalculated size = %d" % (cylinders * 16 * 63 * 512) )
     return (cylinders, 16, 63)
+
 
 def detectFlatImage(filename):
     result = False
-    fin  = open(filename, "rb")
+    fin = open(filename, "rb")
     fin.seek(0x1BC)
     id = struct.unpack(">H", fin.read(2))
     if id[0] == 0x5a5a or id[0] == 0x0000:
@@ -50,88 +58,85 @@ def detectFlatImage(filename):
     fin.close()
     return result
 
+
 def main(argv):
-  print("==========================================")
-  print("Amiga Bridgeboard Aboot HDD file converter\n(V0.2 by Heiko Pruessing in 2021,2022)\n")
-  if len(argv) <= 1:
-      print("Usage: %s <HDDimagefile>" % (argv[0]))
-      exit()
-  filename=argv[1]
+    print("==========================================")
+    print("Amiga Bridgeboard Aboot HDD file converter\n(V0.2 by Heiko Prüssing in 2021,2022)\n")
+    if len(argv) <= 1:
+        print("Usage: %s <HDDimagefile>" % (argv[0]))
+        exit()
+    filename = argv[1]
 
-  print("Using file: %s" % filename)
+    print("Using file: %s" % filename)
 
-  # Check if it is a ABOOT Amiga image or raw HDD image (from bochs or DOSBox)
-  fin  = open(filename, "rb")
-  abootHeader = struct.unpack(">8sHHH", fin.read(14))
-  if abootHeader[0] == b"ABOOT\0\0\0":
+    # Check if it is a ABOOT Amiga image or raw HDD image (from bochs or DOSBox)
+    fin = open(filename, "rb")
+    abootHeader = struct.unpack(">8sHHH", fin.read(14))
+    if abootHeader[0] == b"ABOOT\0\0\0":
 
-      # Aboot HDD file:
-      heads = abootHeader[1]
-      sectors = abootHeader[2]
-      cylinders = abootHeader[3]
+        # Aboot HDD file:
+        heads = abootHeader[1]
+        sectors = abootHeader[2]
+        cylinders = abootHeader[3]
 
-      print("Detected file type: 'Amiga PC ABOOT HDD image file'.")
-      print("Detected geometry: Cylinders=%d heads=%d sectors=%d" % (cylinders, heads, sectors))
-      print("Convert to a DOSBox / Bochs HDD image ('flat raw HHD image')...")
-      if True:
-          filenameout = filename.split(".")[0]+".img-out"
-          ofilestring = input( 'Output file name? (default \'{}\'): '.format(filenameout) )
-          if ofilestring == "":
-              ofilestring = filenameout
+        print("Detected file type: 'Amiga PC ABOOT HDD image file'.")
+        print("Detected geometry: Cylinders=%d heads=%d sectors=%d" % (cylinders, heads, sectors))
+        print("Convert to a DOSBox / Bochs HDD image ('flat raw HHD image')...")
+        
+        filenameout = filename.split(".")[0]+".img-out"
+        ofilestring = input('Output file name? (default \'{}\'): '.format(filenameout))
+        if ofilestring == "":
+            ofilestring = filenameout
 
-          fout=open(filenameout, "wb")
-          fout.write(fin.read())
-          fout.close()
-          print("Success. You can mount the image now with DOSBox command:\n")
-          print(" IMGMOUNT C %s -size 512,%d,%d,%d -t hdd\n" % (filenameout,sectors,heads,cylinders) )
+        fout = open(filenameout, "wb")
+        fout.write(fin.read())
+        fout.close()
+        print("Successfully. You can mount the image now with DOSBox command:\n")
+        print(" IMGMOUNT C %s -size 512,%d,%d,%d -t hdd\n" % (filenameout, sectors, heads, cylinders))
 
-  elif detectFlatImage(filename):
-      # Raw HDD image (from Bochs or DOSBOX)
-      print("Detected file type: 'flat HDD image Bochs / DOSBox'.")
-      print("Converting file to a Commodore Bridgeboard autoboot harddisk image file...")
-      #ch = input("Should the file be converted to a Amiga PC virtual HDD image (usable as hard disk for a Amiga Bridgeboard) [y/n]? ")
-      #if ch == 'y':
-      if True:
-          filenameout = filename.split(".")[0]+".aboot-out"
-          ofilestring = input( 'Output file name? (default \'{}\'): '.format(filenameout) )
-          if ofilestring == "":
-              ofilestring = filenameout
+    elif detectFlatImage(filename):
+        # Raw HDD image (from Bochs or DOSBOX)
+        print("Detected file type: 'flat HDD image Bochs / DOSBox'.")
+        print("Converting file to a Commodore Bridgeboard autoboot harddisk image file...")
 
-          # Try to detecting the CHS geometry from the MBR in the file
-          # detectExistingCHSGeometryFromMBR(filename)
-          chs = detectFlatHardDiskGeometry(filename)
+        filenameout = filename.split(".")[0]+".aboot-out"
+        ofilestring = input( 'Output file name? (default \'{}\'): '.format(filenameout) )
+        if ofilestring == "":
+            ofilestring = filenameout
 
-          print("Please enter the hardfile geometry:'")
-          cylinders = inputNumber("Cylinders [1..1024] (default %d) : " % chs[0], min=1, max=1024, default=chs[0])
-          heads     = inputNumber("Headers   [1..16]   (default %d) : " % chs[1], min=1, max=16,   default=chs[1])
-          sectors   = inputNumber("Sectors   [1..64]   (default %d) : " % chs[2], min=1, max=64,   default=chs[2])
+        # Try to detecting the CHS geometry from the MBR of the file
+        chs = detectFlatHardDiskGeometry(filename)
 
-          harddisksize = (sectorSize * heads * sectors * cylinders)
+        print("Please enter the hardfile geometry:'")
+        cylinders = inputNumber("Cylinders [1..1024] (default %d) : " % chs[0], min=1, max=1024, default=chs[0])
+        heads     = inputNumber("Headers   [1..16]   (default %d) : " % chs[1], min=1, max=16,   default=chs[1])
+        sectors   = inputNumber("Sectors   [1..64]   (default %d) : " % chs[2], min=1, max=64,   default=chs[2])
 
-          print("\nResulting hard disk file size: %d (bytes)." % (harddisksize + abootHeaderSize) )
-          print("Usable for DOS will be %d MB." % (harddisksize / 1024 / 1024) )
+        harddisksize = (sectorSize * heads * sectors * cylinders)
 
-          # Insert the "ABOOT header" which is a 512 bytes block in front of the hdd image file
-          fin.close()
-          fin  = open(filename, "rb")
-          fout = open(ofilestring,"wb")
-          fout.write(struct.pack(">8sHHH",
-                                  b"ABOOT\0\0\0",
-                                  heads,
-                                  sectors,
-                                  cylinders))
-          fout.write(bytearray(b'\xf6'*(sectorSize - 14)))
-          # Take over the rest of the hardfile...
-          fout.write(fin.read())
-          fout.close()
-          fin.close()
-          print("Success. Now you can use '%s' as a autoboot harddisk file for your Amiga Bridgeboard. Have fun ;)" % (ofilestring) )
-          exit()
+        print("\nResulting hard disk file size: %d (bytes)." % (harddisksize + abootHeaderSize) )
+        print("Usable for DOS will be %d MB." % (harddisksize / 1024 / 1024) )
 
-  else:
-    print("Unknown harddisk file type...")
+        # Insert the "ABOOT header" which is a 512 bytes block in front of the hdd image file
+        fin.close()
+        fin  = open(filename, "rb")
+        fout = open(ofilestring,"wb")
+        fout.write(struct.pack(">8sHHH",
+                               b"ABOOT\0\0\0",
+                               heads,
+                               sectors,
+                               cylinders))
+        fout.write(bytearray(b'\xf6'*(sectorSize - 14)))
+        # Take over the rest of the hardfile...
+        fout.write(fin.read())
+        fout.close()
+        fin.close()
+        print("Successfully. Now you can use '%s' as a autoboot harddisk file for your Amiga Bridgeboard. Have fun ;)" % (ofilestring) )
 
-  fin.close()
+    else:
+        print("Unknown harddisk file type...")
+
+    fin.close()
 
 
 if __name__ == "__main__":
